dd831fcf09b2eaec69e47561bbe53bdc6a7bdaf0
tintin <tinashekeith24@gmail.com>
2023-05-03 17:21:50 +0200

    initial commit - started new flutter app and added initial packages, linting, assets and folders

---
+++ android/app/src/main/kotlin/com/example/sunny_day/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sunny_day
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/assets.dart
@@ -0,0 +1,17 @@
+
+// ignore_for_file: public_member_api_docs
+
+/// A class that encapsulates the file paths for images in the SunnyDay app
+///
+/// Provides static constants for file paths
+class Assets {
+  // forest themes
+  static const forestCloudy = 'assets/background_images/forest_cloudy.png';
+  static const forestRainy = 'assets/background_images/forest_rainy.png';
+  static const forestSunny = 'assets/background_images/forest_sunny.png';
+
+  // sea themes
+  static const seaCloudy = 'assets/background_images/sea_cloudy.png';
+  static const seaRainy = 'assets/background_images/sea_rainy.png';
+  static const seaSunny = 'assets/background_images/sea_sunny.png';
+}
