16d979623b277cf260a1c741bfc21375c68cf176
tintin <tinashekeith24@gmail.com>
2023-05-06 17:46:24 +0200

    added changing wallpaper and homescreen theme as well as pull to refresh

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,14 +1,7 @@
-// ignore_for_file: public_member_api_docs
-
 /// A class that encapsulates the file paths for images in the SunnyDay app
 ///
 /// Provides static constants for file paths
 class Assets {
-  // forest themes
-  static const forestCloudy = 'assets/backgrounds/forest_cloudy.png';
-  static const forestRainy = 'assets/backgrounds/forest_rainy.png';
-  static const forestSunny = 'assets/backgrounds/forest_sunny.png';
-
   // sea themes
   static const seaCloudy = 'assets/backgrounds/sea_cloudy.png';
   static const seaRainy = 'assets/backgrounds/sea_rainy.png';
---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+class SunnyDayColors {
+  static const sunny = 0xFF4c90e2;
+  static const rainy = 0xFF686868;
+  static const cloudy = 0xFF628594;
+}
