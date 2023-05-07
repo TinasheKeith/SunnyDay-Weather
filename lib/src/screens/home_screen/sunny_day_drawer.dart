9dd7ae61a690a61c1dd4712a2e559549e5885292
tintin <tinashekeith24@gmail.com>
2023-05-07 17:49:10 +0200

    added empty state for drawer

--- lib/src/screens/home_screen/sunny_day_drawer.dart
+++ lib/src/screens/home_screen/sunny_day_drawer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:sunny_day/src/locator.dart';
 import 'package:sunny_day/src/model/saved_location_model.dart';
+import 'package:sunny_day/src/screens/location_search_screen/location_search_screen.dart';
 import 'package:sunny_day/src/services/shared_preferences_service.dart';
 
 class SunnyDayDrawer extends StatefulWidget {
@@ -65,35 +66,95 @@ class _SunnyDayDrawerState extends State<SunnyDayDrawer> {
               indent: 16,
               endIndent: 16,
             ),
-            Expanded(
-              child: ListView.separated(
-                itemCount: _savedLocations.length,
-                separatorBuilder: (context, index) {
-                  return Divider(
-                    color: secondaryColor,
-                    indent: 16,
-                    endIndent: 16,
-                  );
-                },
-                itemBuilder: (context, index) {
-                  final location = _savedLocations[index];
-                  return ListTile(
-                    title: Text(
-                      location.placeName,
-                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                            color: secondaryColor,
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    onTap: () {
-                      Navigator.pop(context, location);
-                    },
-                  );
-                },
+            if (_savedLocations.isEmpty)
+              const _NoSavedLocationsWidget()
+            else
+              Expanded(
+                child: ListView.separated(
+                  itemCount: _savedLocations.length,
+                  separatorBuilder: (context, index) {
+                    return Divider(
+                      color: secondaryColor,
+                      indent: 16,
+                      endIndent: 16,
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    final location = _savedLocations[index];
+                    return ListTile(
+                      title: Text(
+                        location.placeName,
+                        style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                              color: secondaryColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      onTap: () {
+                        Navigator.pop(context, location);
+                      },
+                    );
+                  },
+                ),
               ),
-            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _NoSavedLocationsWidget extends StatelessWidget {
+  const _NoSavedLocationsWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    final secondaryColor = Theme.of(context).colorScheme.secondary;
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 16),
+          Icon(
+            LineIcons.cloudWithHeavyShowers,
+            color: secondaryColor,
+            size: 60,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            "Oops! You haven't saved any locations yet.",
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                  fontSize: 16,
+                ),
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            width: 160,
+            child: MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              color: Theme.of(context).colorScheme.secondary,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(LineIcons.plus),
+                  Text(
+                    'Add a location!',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                Navigator.of(context).popAndPushNamed(LocationSearchScreen.id);
+              },
+            ),
+          )
+        ],
       ),
     );
   }
